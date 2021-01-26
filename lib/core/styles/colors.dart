d0cbd6eb7e3108e84bef0ee834d71e6f01be6c3a
Chandra Abdul Fattah <chandraaf28@gmail.com>
2021-01-26 13:27:15 +0700

    Initial Project

---
+++ lib/core/styles/colors.dart
@@ -0,0 +1,5 @@
+import 'dart:ui';
+
+const colorPrimary = Color(0xff1389FD);
+const green = Color(0xff66bb6a);
+const red = Color(0xfff44336);
---
+++ lib/core/styles/text_styles.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:pexels/core/styles/colors.dart';
+
+const primary18600 = TextStyle(color: colorPrimary, fontSize: 18, fontWeight: FontWeight.w600, fontFamily: "Montserrat");
+const black16 = TextStyle(color: Colors.black, fontSize: 16, fontFamily: "Montserrat");
+const black18 = TextStyle(color: Colors.black, fontSize: 18, fontFamily: "Montserrat");
+const primary14 = TextStyle(color: colorPrimary, fontSize: 14, fontFamily: "Montserrat");
