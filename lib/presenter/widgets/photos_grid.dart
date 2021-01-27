ef15a7a4819aef22b9ff6690774becd7d3ee5e51
Chandra Abdul Fattah <chandraaf28@gmail.com>
2021-01-27 14:45:57 +0700

    Remove Unused function loading in photos_grid

--- lib/presenter/widgets/photos_grid.dart
+++ lib/presenter/widgets/photos_grid.dart
@@ -1,7 +1,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
-import 'package:pexels/core/styles/colors.dart';
 import 'package:pexels/domain/entities/photo.dart';
 import 'package:pexels/presenter/widgets/tile_grid.dart';
 
@@ -32,14 +31,6 @@ class _PhotosGridState extends State<PhotosGrid> {
         return TileGrid(photo);
       },
       staggeredTileBuilder: (index) => StaggeredTile.fit(2),
-    );
-  }
-
-  Widget _loading(){
-    return Center(
-      child: CircularProgressIndicator(
-        valueColor: AlwaysStoppedAnimation<Color>(colorPrimary),
-      ),
     );
   }
 }
